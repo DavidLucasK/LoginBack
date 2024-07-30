489a35ae56af1028bf0060ba9b75bc062b16c49c
DavidLucasK <davidlucasfr70@gmail.com>
2024-07-30 00:00:41 -0300

    Adding variables on vercel

--- auth.js
+++ auth.js
@@ -11,6 +11,15 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+// Configura o transporte de e-mail
+const transporter = nodemailer.createTransport({
+    service: 'gmail', // Ou 'smtp' se estiver usando outro servidor SMTP
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
 // Endpoint para registro
 router.post('/register', async (req, res) => {
     const { email, password } = req.body;
@@ -58,7 +67,6 @@ router.post('/login', async (req, res) => {
             .single();
 
         if (error && error.code === 'PGRST116') {
-            // Se o erro for "PGRST116" significa que não encontrou o usuário
             return res.status(400).json({ message: `Senha incorreta para ${email} ou email não cadastrado` });
         } else if (error) {
             throw error;
@@ -98,24 +106,12 @@ router.post('/forgot', async (req, res) => {
             return res.status(400).json({ message: 'Usuário não encontrado' });
         }
 
-        // Gera um token de redefinição de senha
         const token = crypto.randomBytes(32).toString('hex');
 
-        // Salva o token no banco de dados
         await supabase
             .from('password_resets')
             .insert([{ email, token, created_at: new Date() }]);
 
-        // Configura o transporte de e-mail
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-        });
-
-        // Configura o e-mail
         const mailOptions = {
             from: process.env.EMAIL,
             to: email,
@@ -123,7 +119,6 @@ router.post('/forgot', async (req, res) => {
             text: `Você solicitou a redefinição de senha da sua conta. Clique no link para redefinir: ${process.env.FRONTEND_URL}/reset-password.html?token=${token}&email=${email}`,
         };
 
-        // Envia o e-mail
         transporter.sendMail(mailOptions, (err, info) => {
             if (err) {
                 console.error('Erro ao enviar e-mail:', err);
@@ -144,7 +139,6 @@ router.post('/reset', async (req, res) => {
     const { email, token, newPassword } = req.body;
 
     try {
-        // Verifica o token
         const { data: resetRequest, error } = await supabase
             .from('password_resets')
             .select('*')
@@ -156,7 +150,6 @@ router.post('/reset', async (req, res) => {
             return res.status(400).json({ message: 'Token inválido ou expirado' });
         }
 
-        // Atualiza a senha do usuário
         const hashedPassword = await bcrypt.hash(newPassword, 10);
         const { error: updateError } = await supabase
             .from('users')
@@ -167,7 +160,6 @@ router.post('/reset', async (req, res) => {
             throw updateError;
         }
 
-        // Remove o token após o uso
         await supabase
             .from('password_resets')
             .delete()
