0bd912d1fd587b8b5eb085b5e7bd546dcf98ec55
DavidLucasK <davidlucasfr70@gmail.com>
2024-10-01 12:00:33 -0300

    get endpoint profile by username

--- auth.js
+++ auth.js
@@ -1362,7 +1362,7 @@ router.get('/get_profile_username/:userName', async (req, res) => {
 
         // Verifica se nenhum dado foi retornado
         if (!data || data.length === 0) {
-            return res.status(404).json({ message: 'Usuário não encontrado.' });
+            return res.status(202).json({ message: 'Usuário não encontrado.' });
         }
 
         // Como estamos esperando um único perfil, pegue o primeiro item do array
