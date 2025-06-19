3c8d6e75a2a992d226db4bac07cd43d5f6592ed8
DavidLucasK <davidlucasfr70@gmail.com>
2025-06-19 01:12:15 -0300

    adding new localhsot to CORS

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: ['https://logindl.vercel.app', 'https://teamoma.vercel.app', 'https://loveuportfolio.vercel.app', 'http://127.0.0.1:5500']
-}));
+app.use(
+  cors({
+    origin: [
+      "https://logindl.vercel.app",
+      "https://teamoma.vercel.app",
+      "https://loveuportfolio.vercel.app",
+      "http://127.0.0.1:5500",
+      "http://localhost:8081",
+    ],
+  })
+);
 app.use(express.json());
 
-const authRoutes = require('./auth');
-app.use('/api/auth', authRoutes);
+const authRoutes = require("./auth");
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
